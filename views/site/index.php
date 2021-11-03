986aeaa6003a0ed9bb143ac87aeb038c3d370a43
Disandman <67182702+Disandman@users.noreply.github.com>
2021-11-03 20:20:24 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -2,7 +2,7 @@
 <div class="list-group text-center">
             <h1> </h1>
             <hr>
-            <p class="lead">Простое MVC-приложение</p>
+            <p class="lead">Простое MVC-приложение.</p>
             <a href="/"><img src="/img/extjs-mvc.png" style="width: 60rem;"></a>
             <hr>
         
