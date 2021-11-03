2d09298744eb96bf7e2f7277ccc7229347e299ce
Disandman <67182702+Disandman@users.noreply.github.com>
2021-11-03 22:34:06 +0300

    Update 20211016145426_my_migration.php

--- migrations/migrations/20211016145426_my_migration.php
+++ migrations/migrations/20211016145426_my_migration.php
@@ -7,11 +7,11 @@ final class MyMigration extends AbstractMigration
 {
     public function change()
     {
-        $table = $this->table('user');
-        $table->addColumn('login', 'string', ['limit' => 255])
-            ->addColumn('email', 'string', ['limit' => 255])
-            ->addColumn('password', 'string', ['limit' => 255])
-            ->create();
+        // $table = $this->table('user');
+        // $table->addColumn('login', 'string', ['limit' => 255])
+        //     ->addColumn('email', 'string', ['limit' => 255])
+        //     ->addColumn('password', 'string', ['limit' => 255])
+        //     ->create();
     }
 
 }
