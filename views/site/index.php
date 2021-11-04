e46d87f903cb55e400577c7dc5b06ba3855b0314
Disandman <67182702+Disandman@users.noreply.github.com>
2021-11-04 09:22:04 +0300

    Исправления от 4.11.2021

--- views/site/index.php
+++ views/site/index.php
@@ -3,7 +3,7 @@
             <h1> </h1>
             <hr>
             <p class="lead">Простое MVC-приложение.</p>
-            <a href="/"><img src="/img/extjs-mvc.png" style="width: 60rem;"></a>
+            <a href="/"><img src="/img/extjs-mvc.png" class="img-fluid" style="width: 60rem;"></a>
             <hr>
         
     </div>
